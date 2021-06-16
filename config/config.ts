b856b010aa440ceaa9ae2b9e032e3c875b8a5841
zhang.qiang <zhang.qiang@alibaba-inc.com>
2021-06-16 11:29:15 +0800

    fix: publicPath

--- config/config.ts
+++ config/config.ts
@@ -6,6 +6,7 @@ import proxy from './proxy';
 import routes from './routes';
 const { REACT_APP_ENV } = process.env;
 export default defineConfig({
+  runtimePublicPath: true,
   history: {
     type: 'hash',
   },
